d8b933261b2c2343ffbb73cde7cd1847b56a5b01
gkorol <korol.guilherme@gmail.com>
2020-10-21 11:37:29 -0300

    ...

--- cifar.py
+++ cifar.py
@@ -257,7 +257,8 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
-        losses.update(loss.data[0], inputs.size(0))
+        # losses.update(loss.data[0], inputs.size(0))
+        losses.update(loss.data, inputs.size(0))
         top1.update(prec1[0], inputs.size(0))
         top5.update(prec5[0], inputs.size(0))
 
@@ -314,7 +315,8 @@ def test(testloader, model, criterion, epoch, use_cuda):
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
-        losses.update(loss.data[0], inputs.size(0))
+        # losses.update(loss.data[0], inputs.size(0))
+        losses.update(loss.data, inputs.size(0))
         top1.update(prec1[0], inputs.size(0))
         top5.update(prec5[0], inputs.size(0))
 
